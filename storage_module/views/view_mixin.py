c9d0431f32627c0118ffb65819009d9d368c5120
nmunatsibw <mcturnergarai@gmail.com>
2024-07-08 12:12:25 +0200

    Updated CSV headers and sample details in view_mixin
    This commit expands the information that's included in our CSV export functionality. It adds additional headers and corresponding sample details such as 'Condition', 'Created By', 'Participant ID', 'Gender', 'Date of Birth', 'Time Sampled', 'Requisition ID', 'Protocol Number' to the CSV file to provide users with more comprehensive sample information.

--- storage_module/views/view_mixin.py
+++ storage_module/views/view_mixin.py
@@ -15,8 +15,10 @@ class ViewMixin:
             'Content-Disposition'] = f'attachment; filename=samples_{timestamp_str}.csv'
 
         writer = csv.writer(response)
-        headers = ['Sample ID', 'Sample Type', 'Facility Name', 'Source File Name',
-                   'Box Name', 'Time Sampled', 'Sample Status']
+        headers = ['Sample ID', 'Sample Type', 'Source File Name', 'Condition',
+                   'Created By', 'Participant ID', 'Gender', 'Date of Birth',
+                   'Date Sampled', 'Time Sampled', 'Requisition ID', 'Protocol Number',
+                   'Facility Name', 'Box Name', 'Sample Status']
         writer.writerow(headers)
 
         samples = DimSample.objects.filter(sample_id__in=sample_ids)
@@ -24,10 +26,18 @@ class ViewMixin:
             writer.writerow([
                 sample.sample_id,
                 sample.sample_type.sample_type if sample.sample_type else "",
-                sample.facility.facility_name if sample.facility else "",
                 sample.source_file.source_file_name if sample.source_file else "",
+                sample.sample_condition if sample.sample_condition else "",
+                sample.user_created if sample.user_created else "",
+                sample.participant_id if sample.participant_id else "",
+                sample.gender if sample.gender else "",
+                sample.date_of_birth if sample.date_of_birth else "",
+                sample.date_sampled if sample.date_sampled else "",
+                sample.time_sampled if sample.time_sampled else "",
+                sample.requisition_id if sample.requisition_id else "",
+                sample.protocol_number if sample.protocol_number else "",
+                sample.facility.facility_name if sample.facility else "",
                 sample.box.box_name if sample.box else "",
-                sample.date_sampled,
                 sample.sample_status.name if sample.sample_status else ""
             ])
 
